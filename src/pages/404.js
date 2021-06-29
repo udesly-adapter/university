f02571f88fc7a6cf0f01d31efa10ad12d9b0dc48
EclipseSrl <dev@eclipse.srl>
2021-06-29 11:51:02 +0200

    Fixed 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,29 +1,22 @@
 import * as React from "react"
-import { graphql } from "gatsby"
-
-import Layout from "../components/layout"
+import { Link } from "gatsby"
 import SEO from "../components/seo"
+import logo from '../../static/images/logo.svg';
 
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
+const NotFoundPage = ({ data }) => {
 
   return (
-    <Layout location={location} title={siteTitle}>
+    <>
       <SEO title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", height: "100vh" }}>
+        <img src={logo} alt="logo" width="200px" height="auto"/>
+        <h1 style={{fontWeight: "normal", marginTop: "30px"}}>404: Not Found</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <Link className="main-button" to="/">Back to Home</Link>
+      </div>
+
+    </>
   )
 }
 
 export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
