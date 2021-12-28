db38c0956daa34c2ec5f848746cdeb51dfe11aa1
EclipseSrl <dev@eclipse.srl>
2021-12-28 11:51:38 +0100

    Init Ghost documentation

---
+++ docs/ghost/01 - Getting Started/03 - conversion steps.md
@@ -0,0 +1,41 @@
+---
+title: Conversion Steps
+description: Conversion steps
+---
+
+Converting a site is very very simple, below you will find a guide divided into 3 simple parts
+
+## Webflow
+
+1.  Design your theme in Webflow, with the correct CMS structure; 
+2.  After you are done and ready to convert, open up our extension and click on **Configure Attributes**;
+3.  The extension will add attributes and download a configuration file;
+4.  After the configuration has been downloaded you can export your Webflow *.zip*;
+
+<small>* You can also get a template from the Marketplace, possibly made by Udesly 🥰</small>
+
+![](/images/wf-to-jamstack-attributes.png)
+
+## Udesly App
+
+1. Upload your zip file into the Udesly App;
+2. Add the configuration file and configure the settings;
+3. Press on **Convert**;
+4. At the end of the conversion the Udesly App will allows you to either download the zip or upload it to Github;
+
+![](/images/netlify-convert.png)
+
+
+### Download Theme
+
+Press this button to download the Ghost theme zip.
+
+### Download Data
+
+Press this button to download the data with CMS elements and pages that you can import into Ghost platform
+
+### Download Routes
+
+Press this button to download the routes.yaml file that controls how your Ghost CMS will route the static and cms pages
+
+
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,31 +1,81 @@
-import React from 'react';
-import './hero.css';
-import { Link } from 'gatsby';
-import heroImage from '../../../static/images/hero-image.jpg'
+import React from "react"
+import "./hero.css"
+import { Link } from "gatsby"
+import heroImage from "../../../static/images/hero-image.jpg"
 
 const Hero = () => {
-
-    return (
-        <div className="hero">
-            <div className="hero-content-wrapper">
-                <div className="hero-text-wrap">
-                    <h1 className="hero-heading">Easy docs, tips & tricks, tutorials...</h1>
-                    <p className="big-paragraph max-width">Use <span className="text-span">Webflow</span> to design your project and <span className="text-span">convert</span> it in a seconds with Udesly, to create professional themes for Shopify, WordPress, Jamstack or Ghost.</p>
-                    <div className="logo-wrapper">
-                        <Link to="/shopify/getting-started" className="logo-container w-inline-block">
-                            <img src="https://uploads-ssl.webflow.com/60acf65ac9328ac27c774303/60acf65ac9328a117e774358_webflow-to-shopify.svg" loading="lazy" alt="Webflow to Shopify" />
-                        </Link>
-                        <Link to="/wordpress/getting-started" className="logo-container w-inline-block">
-                            <img src="https://uploads-ssl.webflow.com/60acf65ac9328ac27c774303/60acf65ac9328ae528774364_webflow-to-wordpress.svg" loading="lazy" alt="Webflow to WordPress" />
-                        </Link>
-                        <Link to="/jamstack/getting-started" className="logo-container w-inline-block">
-                            <img src="https://uploads-ssl.webflow.com/60acf65ac9328ac27c774303/60acf65ac9328ab98277435a_webflow-to-jamstack.svg" loading="lazy" alt="Webflow to Jamstack" />
-                        </Link>
-                        <a href="#" style={{ cursor: "not-allowed" }} className="logo-container disabled w-inline-block"><img src="https://uploads-ssl.webflow.com/60acf65ac9328ac27c774303/60acf65ac9328ab132774363_webflow-to-ghost.svg" loading="lazy" alt="Webflow to Ghost" /></a>
-                    </div><div className="button-container"><a href="https://www.udesly.com/why-udesly" target="_blank" rel="noopener noreferrer" className="arrow-link w-button">Why you should use Udesly </a></div></div><div className="hero-image-wrapper"><img src={heroImage} alt="udesly-hero" className="hero-image" /></div></div>
+  return (
+    <div className="hero">
+      <div className="hero-content-wrapper">
+        <div className="hero-text-wrap">
+          <h1 className="hero-heading">
+            Easy docs, tips & tricks, tutorials...
+          </h1>
+          <p className="big-paragraph max-width">
+            Use <span className="text-span">Webflow</span> to design your
+            project and <span className="text-span">convert</span> it in a
+            seconds with Udesly, to create professional themes for Shopify,
+            WordPress, Jamstack or Ghost.
+          </p>
+          <div className="logo-wrapper">
+            <Link
+              to="/shopify/getting-started"
+              className="logo-container w-inline-block"
+            >
+              <img
+                src="https://uploads-ssl.webflow.com/60acf65ac9328ac27c774303/60acf65ac9328a117e774358_webflow-to-shopify.svg"
+                loading="lazy"
+                alt="Webflow to Shopify"
+              />
+            </Link>
+            <Link
+              to="/wordpress/getting-started"
+              className="logo-container w-inline-block"
+            >
+              <img
+                src="https://uploads-ssl.webflow.com/60acf65ac9328ac27c774303/60acf65ac9328ae528774364_webflow-to-wordpress.svg"
+                loading="lazy"
+                alt="Webflow to WordPress"
+              />
+            </Link>
+            <Link
+              to="/jamstack/getting-started"
+              className="logo-container w-inline-block"
+            >
+              <img
+                src="https://uploads-ssl.webflow.com/60acf65ac9328ac27c774303/60acf65ac9328ab98277435a_webflow-to-jamstack.svg"
+                loading="lazy"
+                alt="Webflow to Jamstack"
+              />
+            </Link>
+            <Link
+              to="/ghost/getting-started"
+              className="logo-container w-inline-block"
+            >
+              <img
+                src="https://uploads-ssl.webflow.com/60acf65ac9328ac27c774303/60acf65ac9328ab132774363_webflow-to-ghost.svg"
+                loading="lazy"
+                alt="Webflow to Ghost"
+              />
+            </Link>
+          </div>
+          <div className="button-container">
+            <a
+              href="https://www.udesly.com/why-udesly"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="arrow-link w-button"
+            >
+              Why you should use Udesly{" "}
+            </a>
+          </div>
         </div>
-    );
-
+        <div className="hero-image-wrapper">
+          <img src={heroImage} alt="udesly-hero" className="hero-image" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Hero;
+export default Hero
